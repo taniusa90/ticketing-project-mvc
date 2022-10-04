8c08b9a4aa7919c2da4cc7711143ba58893ef3c3
taniusa90 <galea_tanea@mail.com>
2022-10-04 16:09:43 -0500

    Task Create

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,11 +1,14 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/project")
@@ -76,6 +79,24 @@ public class ProjectController {
         projectService.update(project);
         return "redirect:/project/create";
     }
+
+    @GetMapping("/manager/project-status")
+    public String getProjectByManager(Model model){
+
+        UserDTO manager = userService.findById("john@cydeo.com");
+
+
+        List<ProjectDTO> projects = projectService.getCountedListOfProjectDTO(manager);
+
+
+        model.addAttribute("projects",projects);
+
+
+        return "/manager/project-status";
+
+    }
+
+
 
 
 
